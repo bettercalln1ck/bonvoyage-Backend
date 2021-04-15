110cb54e886752e0dbab4c8f4765bb21487506a0
nikolai6303 <iit2018152@iiita.ac.in>
2021-04-16 01:39:20 +0530

    added trip Router

---
+++ models/user.js
@@ -0,0 +1,38 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+
+var passportLocalMongoose=require('passport-local-mongoose');
+
+var User=new Schema({
+	firstname:{
+		type:String,
+		default:'',
+	},
+	lastname:{
+		type:String,
+		default:'',
+	},
+	email:{
+		type:String,
+		default:'',
+	},
+	dateofbirth: {
+		type: String,
+		default: ''
+	},
+	bio: {
+		type: String,
+		default: ''
+	},
+	image: {
+		type: String,
+		default: ''
+	}
+    },
+    {
+        timestamps: true
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports=mongoose.model('User',User);
--- routes/tripsRouter.js
+++ routes/tripsRouter.js
@@ -42,9 +42,32 @@ tripsRouter.route('/makeTrip')
   Trips.create((req.body))
   .then((trip) =>{
     src = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+trip.cityName+"&type=tourist_attraction&rankby=prominence&key=AIzaSyArM7cAmAWdHA2I6iL0XLLo979LOyy-920"
+    dataX = []
 
-    
+    axios.get(src)
+      .then(async(response) => {
 
+        await Promise.all( response.data.results.map( async (element) => {
+            src2 = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + element.place_id + "&fields=opening_hours&key=AIzaSyArM7cAmAWdHA2I6iL0XLLo979LOyy-920"
+            console.log(src2)
+            await axios.get(src2).then(async (resp) => {
+                if(!(resp.data.result.opening_hours == null)){
+                    await dataX.push({place_id:element.place_id,  formatted_address: element.formatted_address, name:element.name, geometry: element.geometry, rating: element.rating, user_ratings_total: element.user_ratings_total, types: element.types ,  opening_hours: resp.data.result.opening_hours.periods})
+                }
+            }).catch(error => {
+                console.log(error);
+                return next(err); 
+            });
+
+      })
+        )
+        console.log(dataX)
+      await res.status(200).json(dataX);
+  })
+  .catch(error => {
+    console.log(error);
+    return next(err); 
+  })
   }, (err) => next(err))
     .catch((err) => next(err));
 
