e89c4ef8eebbf607801f89d71025f77bbeb8e4f3
nikolai6303 <iit2018152@iiita.ac.in>
2021-05-23 04:42:47 +0530

    mend

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb+srv://admin:python6303@cluster0.wodzq.mongodb.net/merch?retryWrites=true&w=majority',
+    'mongoUrl' : 'mongodb+srv://n1kolai:python6303@cluster0.bimoq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  //  'mongoUrl': 'mongodb+srv://admin:python6303@cluster0.wodzq.mongodb.net/merch?retryWrites=true&w=majority',
 }
